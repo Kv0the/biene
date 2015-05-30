0c14e10fdf619f8795a130032189ff250d566c13
Kv0the <elnombredelviento@gmail.com>
2015-05-30 18:30:43 +0200

    Appu

--- project/app/src/main/java/angelhack/biene/MainActivity.java
+++ project/app/src/main/java/angelhack/biene/MainActivity.java
@@ -1,9 +1,14 @@
 package angelhack.biene;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +17,26 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ActionBarActivity context = this;
+
+        Button st_journey = (Button) context.findViewById(R.id.btn_start_journey);
+
+        st_journey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Journey.class);
+                startActivity(intent);
+            }
+        });
+
+        Button rev_journeys = (Button) this.findViewById(R.id.btn_review_journeys);
+
+        rev_journeys.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "REVIEW", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
